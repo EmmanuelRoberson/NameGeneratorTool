7c754fb03d3664ce19090095b8521e4175b5e4ba
emmanuel.roberson3 <emmanuel.roberson3@gmail.com>
2019-03-15 10:53:36 -0500

    before feedback

--- NameGenerator/NameGeneratorProject/Form1.Designer.cs
+++ NameGenerator/NameGeneratorProject/Form1.Designer.cs
@@ -40,12 +40,26 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.nameSpecificationsGroupBox = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.availableHonorifics = new System.Windows.Forms.ComboBox();
             this.honorificTypeLabel = new System.Windows.Forms.Label();
             this.nameNationalityLabel = new System.Windows.Forms.Label();
             this.honorificComboBox = new System.Windows.Forms.ComboBox();
+            this.addTypesOfSpecificationsGroupbox = new System.Windows.Forms.GroupBox();
+            this.addNameNationalityButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.nameGeneratorTab = new System.Windows.Forms.TabControl();
+            this.speficicationsTab = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.panel1.SuspendLayout();
             this.nameSpecificationsGroupBox.SuspendLayout();
+            this.addTypesOfSpecificationsGroupbox.SuspendLayout();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.nameGeneratorTab.SuspendLayout();
+            this.speficicationsTab.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // saveNameButton
@@ -56,9 +70,9 @@
             this.saveNameButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSeaGreen;
             this.saveNameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.saveNameButton.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saveNameButton.Location = new System.Drawing.Point(395, 106);
+            this.saveNameButton.Location = new System.Drawing.Point(15, 110);
             this.saveNameButton.Name = "saveNameButton";
-            this.saveNameButton.Size = new System.Drawing.Size(131, 38);
+            this.saveNameButton.Size = new System.Drawing.Size(155, 38);
             this.saveNameButton.TabIndex = 1;
             this.saveNameButton.Text = "SAVE NAME";
             this.saveNameButton.UseVisualStyleBackColor = false;
@@ -82,7 +96,7 @@
             // 
             this.maleSymbolButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("maleSymbolButton.BackgroundImage")));
             this.maleSymbolButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.maleSymbolButton.Location = new System.Drawing.Point(283, 106);
+            this.maleSymbolButton.Location = new System.Drawing.Point(502, 94);
             this.maleSymbolButton.Name = "maleSymbolButton";
             this.maleSymbolButton.Size = new System.Drawing.Size(42, 38);
             this.maleSymbolButton.TabIndex = 27;
@@ -93,7 +107,7 @@
             // 
             this.femaleSymbolButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("femaleSymbolButton.BackgroundImage")));
             this.femaleSymbolButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.femaleSymbolButton.Location = new System.Drawing.Point(347, 106);
+            this.femaleSymbolButton.Location = new System.Drawing.Point(483, 138);
             this.femaleSymbolButton.Name = "femaleSymbolButton";
             this.femaleSymbolButton.Size = new System.Drawing.Size(42, 38);
             this.femaleSymbolButton.TabIndex = 28;
@@ -105,7 +119,7 @@
             this.japaneseNameGeneratorButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("japaneseNameGeneratorButton.BackgroundImage")));
             this.japaneseNameGeneratorButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.japaneseNameGeneratorButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.japaneseNameGeneratorButton.Location = new System.Drawing.Point(283, 150);
+            this.japaneseNameGeneratorButton.Location = new System.Drawing.Point(448, 39);
             this.japaneseNameGeneratorButton.Name = "japaneseNameGeneratorButton";
             this.japaneseNameGeneratorButton.Size = new System.Drawing.Size(106, 58);
             this.japaneseNameGeneratorButton.TabIndex = 29;
@@ -121,9 +135,9 @@
             this.randomlyGeneratedNameButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SteelBlue;
             this.randomlyGeneratedNameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.randomlyGeneratedNameButton.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.randomlyGeneratedNameButton.Location = new System.Drawing.Point(283, 44);
+            this.randomlyGeneratedNameButton.Location = new System.Drawing.Point(15, 7);
             this.randomlyGeneratedNameButton.Name = "randomlyGeneratedNameButton";
-            this.randomlyGeneratedNameButton.Size = new System.Drawing.Size(106, 100);
+            this.randomlyGeneratedNameButton.Size = new System.Drawing.Size(155, 67);
             this.randomlyGeneratedNameButton.TabIndex = 31;
             this.randomlyGeneratedNameButton.Text = "Generate";
             this.randomlyGeneratedNameButton.UseVisualStyleBackColor = false;
@@ -133,9 +147,9 @@
             // 
             this.nameGeneratedText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.nameGeneratedText.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameGeneratedText.Location = new System.Drawing.Point(395, 77);
+            this.nameGeneratedText.Location = new System.Drawing.Point(15, 75);
             this.nameGeneratedText.Name = "nameGeneratedText";
-            this.nameGeneratedText.Size = new System.Drawing.Size(131, 23);
+            this.nameGeneratedText.Size = new System.Drawing.Size(155, 23);
             this.nameGeneratedText.TabIndex = 32;
             this.nameGeneratedText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.nameGeneratedText.TextChanged += new System.EventHandler(this.nameGeneratedText_TextChanged);
@@ -149,7 +163,7 @@
             this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.exitButton.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.exitButton.ForeColor = System.Drawing.Color.White;
-            this.exitButton.Location = new System.Drawing.Point(535, 2);
+            this.exitButton.Location = new System.Drawing.Point(198, 2);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(27, 24);
             this.exitButton.TabIndex = 36;
@@ -165,8 +179,12 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(530, 29);
+            this.panel1.Size = new System.Drawing.Size(742, 29);
             this.panel1.TabIndex = 37;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
             // label1
             // 
@@ -181,18 +199,28 @@
             // 
             // nameSpecificationsGroupBox
             // 
+            this.nameSpecificationsGroupBox.Controls.Add(this.label2);
             this.nameSpecificationsGroupBox.Controls.Add(this.availableHonorifics);
             this.nameSpecificationsGroupBox.Controls.Add(this.honorificTypeLabel);
             this.nameSpecificationsGroupBox.Controls.Add(this.nameNationalityLabel);
             this.nameSpecificationsGroupBox.Controls.Add(this.honorificComboBox);
             this.nameSpecificationsGroupBox.Controls.Add(this.nationalitiesDropDox);
             this.nameSpecificationsGroupBox.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameSpecificationsGroupBox.Location = new System.Drawing.Point(8, 35);
+            this.nameSpecificationsGroupBox.Location = new System.Drawing.Point(6, 6);
             this.nameSpecificationsGroupBox.Name = "nameSpecificationsGroupBox";
-            this.nameSpecificationsGroupBox.Size = new System.Drawing.Size(269, 109);
+            this.nameSpecificationsGroupBox.Size = new System.Drawing.Size(301, 96);
             this.nameSpecificationsGroupBox.TabIndex = 38;
             this.nameSpecificationsGroupBox.TabStop = false;
             this.nameSpecificationsGroupBox.Text = "Name Specifications";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 16);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Available Honorifics";
             // 
             // availableHonorifics
             // 
@@ -235,24 +263,114 @@
             this.honorificComboBox.TabIndex = 27;
             this.honorificComboBox.SelectedIndexChanged += new System.EventHandler(this.honorificComboBox_SelectedIndexChanged);
             // 
+            // addTypesOfSpecificationsGroupbox
+            // 
+            this.addTypesOfSpecificationsGroupbox.Controls.Add(this.textBox1);
+            this.addTypesOfSpecificationsGroupbox.Controls.Add(this.addNameNationalityButton);
+            this.addTypesOfSpecificationsGroupbox.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addTypesOfSpecificationsGroupbox.Location = new System.Drawing.Point(6, 6);
+            this.addTypesOfSpecificationsGroupbox.Name = "addTypesOfSpecificationsGroupbox";
+            this.addTypesOfSpecificationsGroupbox.Size = new System.Drawing.Size(321, 112);
+            this.addTypesOfSpecificationsGroupbox.TabIndex = 39;
+            this.addTypesOfSpecificationsGroupbox.TabStop = false;
+            this.addTypesOfSpecificationsGroupbox.Text = "Add Your Own";
+            // 
+            // addNameNationalityButton
+            // 
+            this.addNameNationalityButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.addNameNationalityButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.addNameNationalityButton.FlatAppearance.BorderSize = 0;
+            this.addNameNationalityButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSeaGreen;
+            this.addNameNationalityButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addNameNationalityButton.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addNameNationalityButton.Location = new System.Drawing.Point(142, 19);
+            this.addNameNationalityButton.Name = "addNameNationalityButton";
+            this.addNameNationalityButton.Size = new System.Drawing.Size(108, 24);
+            this.addNameNationalityButton.TabIndex = 2;
+            this.addNameNationalityButton.Text = "Add  Nationality";
+            this.addNameNationalityButton.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(6, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.randomlyGeneratedNameButton);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.saveNameButton);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.nameGeneratedText);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(182, 302);
+            this.toolStripContainer1.ContentPanel.Load += new System.EventHandler(this.toolStripContainer1_ContentPanel_Load);
+            this.toolStripContainer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripContainer1.Location = new System.Drawing.Point(560, 28);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(182, 302);
+            this.toolStripContainer1.TabIndex = 40;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // nameGeneratorTab
+            // 
+            this.nameGeneratorTab.Controls.Add(this.speficicationsTab);
+            this.nameGeneratorTab.Controls.Add(this.tabPage2);
+            this.nameGeneratorTab.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.nameGeneratorTab.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameGeneratorTab.Location = new System.Drawing.Point(12, 39);
+            this.nameGeneratorTab.Name = "nameGeneratorTab";
+            this.nameGeneratorTab.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.nameGeneratorTab.SelectedIndex = 0;
+            this.nameGeneratorTab.Size = new System.Drawing.Size(465, 137);
+            this.nameGeneratorTab.TabIndex = 41;
+            this.nameGeneratorTab.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.nameGeneratorTab_DrawItem);
+            // 
+            // speficicationsTab
+            // 
+            this.speficicationsTab.BackColor = System.Drawing.Color.White;
+            this.speficicationsTab.Controls.Add(this.nameSpecificationsGroupBox);
+            this.speficicationsTab.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.speficicationsTab.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.speficicationsTab.Location = new System.Drawing.Point(4, 25);
+            this.speficicationsTab.Name = "speficicationsTab";
+            this.speficicationsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.speficicationsTab.Size = new System.Drawing.Size(457, 108);
+            this.speficicationsTab.TabIndex = 0;
+            this.speficicationsTab.Text = "Specifications";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.addTypesOfSpecificationsGroupbox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(457, 108);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // nameGeneratorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(530, 152);
-            this.Controls.Add(this.nameSpecificationsGroupBox);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.nameGeneratedText);
-            this.Controls.Add(this.randomlyGeneratedNameButton);
-            this.Controls.Add(this.japaneseNameGeneratorButton);
+            this.ClientSize = new System.Drawing.Size(742, 192);
+            this.Controls.Add(this.nameGeneratorTab);
+            this.Controls.Add(this.toolStripContainer1);
             this.Controls.Add(this.femaleSymbolButton);
             this.Controls.Add(this.maleSymbolButton);
-            this.Controls.Add(this.saveNameButton);
+            this.Controls.Add(this.japaneseNameGeneratorButton);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "nameGeneratorForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "NameGeneratorTool";
+            this.Text = "\\";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.nameGeneratorForm_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.nameGeneratorForm_MouseMove);
@@ -260,8 +378,16 @@
             this.panel1.PerformLayout();
             this.nameSpecificationsGroupBox.ResumeLayout(false);
             this.nameSpecificationsGroupBox.PerformLayout();
+            this.addTypesOfSpecificationsGroupbox.ResumeLayout(false);
+            this.addTypesOfSpecificationsGroupbox.PerformLayout();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.nameGeneratorTab.ResumeLayout(false);
+            this.speficicationsTab.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -281,5 +407,13 @@
         private System.Windows.Forms.Label honorificTypeLabel;
         private System.Windows.Forms.Label nameNationalityLabel;
         private System.Windows.Forms.ComboBox availableHonorifics;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox addTypesOfSpecificationsGroupbox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button addNameNationalityButton;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.TabControl nameGeneratorTab;
+        private System.Windows.Forms.TabPage speficicationsTab;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
--- NameGenerator/NameGeneratorProject/Form1.cs
+++ NameGenerator/NameGeneratorProject/Form1.cs
@@ -26,6 +26,8 @@ namespace NameGeneratorProject
         public NameGeneratorContainer nameGeneratorList;
         public List<HonorificList> honorificListContainer;
 
+
+
         public nameGeneratorForm()
         {
             InitializeComponent();
@@ -36,6 +38,8 @@ namespace NameGeneratorProject
                 suffixes = new AppendList()
             };
 
+            //ToDo:: namegeneratorcontainer to have a constructor for multiple instances
+            
             nameGeneratorList = new NameGeneratorContainer();
             nameGeneratorList.AddNameGenerator("AmericanFirstnames.csv", "American");
             nameGeneratorList.AddNameGenerator("HispanicFirstnames.csv", "Hispanic");
@@ -43,12 +47,14 @@ namespace NameGeneratorProject
             nameGeneratorList.AddNameGenerator("FrenchFirstnames.csv", "French");
             nameGeneratorList.AddNameGenerator("RussianFirstnames.csv", "Russian");
 
-            honorificListContainer = new List<HonorificList>();
-            honorificListContainer.Add(new HonorificList("AmericanHonorifics.csv","American"));
-            honorificListContainer.Add(new HonorificList("HispanicHonorifics.csv","Hispanic"));
-            honorificListContainer.Add(new HonorificList("JapaneseHonorifics.csv","Japanese"));
-            honorificListContainer.Add(new HonorificList("FrenchHonorifics.csv","French"));
-            honorificListContainer.Add(new HonorificList("RussianHonorifics.csv","Russian"));
+            honorificListContainer = new List<HonorificList>
+            {
+                new HonorificList("AmericanHonorifics.csv", "American"),
+                new HonorificList("HispanicHonorifics.csv", "Hispanic"),
+                new HonorificList("JapaneseHonorifics.csv", "Japanese"),
+                new HonorificList("FrenchHonorifics.csv", "French"),
+                new HonorificList("RussianHonorifics.csv", "Russian")
+            };
 
 
             int i = 0;
@@ -64,6 +70,8 @@ namespace NameGeneratorProject
                 honorificComboBox.Items.Add(honorificListContainer[i].GetHonorificType);
                 i++;
             }
+
+            exitButton.Show();
         }
 
         //Save Name
@@ -86,34 +94,31 @@ namespace NameGeneratorProject
             japaneseNameGeneratorButton.Visible = false;
         }
 
-        //[No Name Generated]
-
-
         private void nationalitiesDropDox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (!((string)nationalitiesDropDox.SelectedItem == "Randomly Generated"))
-            {
-                randomlyGeneratedNameButton.Hide();
-                maleSymbolButton.Show();
-                femaleSymbolButton.Show();
-            }
-            else
-            {
-                randomlyGeneratedNameButton.Show();
-                maleSymbolButton.Hide();
-                femaleSymbolButton.Hide();
-            }
+            //if (!((string)nationalitiesDropDox.SelectedItem == "Randomly Generated"))
+            //{
+            //    randomlyGeneratedNameButton.Hide();
+            //    maleSymbolButton.Show();
+            //    femaleSymbolButton.Show();
+            //}
+            //else
+            //{
+            //    randomlyGeneratedNameButton.Show();
+            //    maleSymbolButton.Hide();
+            //    femaleSymbolButton.Hide();
+            //}
 
-            if (!(nationalitiesDropDox.SelectedItem == "Japanese"))
-            {
-                this.Size = new Size(this.Size.Width, 150);
-                japaneseNameGeneratorButton.Hide();
-            }
-            else
-            {
-                this.Size = new Size(this.Size.Width, 220);
-                japaneseNameGeneratorButton.Show();
-            }
+            //if (!(nationalitiesDropDox.SelectedItem == "Japanese"))
+            //{
+            //    this.Size = new Size(this.Size.Width, 150);
+            //    japaneseNameGeneratorButton.Hide();
+            //}
+            //else
+            //{
+            //    this.Size = new Size(this.Size.Width, 220);
+            //    japaneseNameGeneratorButton.Show();
+            //}
         }
 
         private void maleSymbolButton_Click(object sender, EventArgs e)
@@ -224,6 +229,48 @@ namespace NameGeneratorProject
                 else
             {
                 nameGenerated = (string)availableHonorifics.SelectedItem + " " + nameGenerated;
+            }
+        }
+ 
+        // used in order to drag the panel to drag the form
+        private bool isDragging = false;
+        private Point offset;
+        private Point start_point = new Point(0, 0);
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;  // isDragging is your variable flag
+            start_point = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this.start_point.X, p.Y - this.start_point.Y);
+            }
+        }
+
+        private void toolStripContainer1_ContentPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameGeneratorTab_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            for (int i = 0; i < nameGeneratorTab.TabCount; i++)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Salmon), e.Bounds);
             }
         }
     }
