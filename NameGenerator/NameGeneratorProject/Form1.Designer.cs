ffd87caadc71ff2eb87804fdfa3fe96575965858
emmanuel.roberson3 <emmanuel.roberson3@gmail.com>
2019-03-27 11:09:33 -0500

    Changed to only have one button and a text box

--- NameGenerator/NameGeneratorProject/Form1.Designer.cs
+++ NameGenerator/NameGeneratorProject/Form1.Designer.cs
@@ -29,47 +29,27 @@
         private void InitializeComponent()
         {
             this.randomGeneratedNameButton = new System.Windows.Forms.Button();
-            this.saveNameButton = new System.Windows.Forms.Button();
-            this.nameGeneratedButton = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nameGeneratedTextBox = new System.Windows.Forms.RichTextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // randomGeneratedNameButton
             // 
-            this.randomGeneratedNameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.randomGeneratedNameButton.Location = new System.Drawing.Point(12, 42);
+            this.randomGeneratedNameButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.randomGeneratedNameButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.randomGeneratedNameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.randomGeneratedNameButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.randomGeneratedNameButton.Location = new System.Drawing.Point(12, 38);
             this.randomGeneratedNameButton.Name = "randomGeneratedNameButton";
-            this.randomGeneratedNameButton.Size = new System.Drawing.Size(93, 38);
+            this.randomGeneratedNameButton.Size = new System.Drawing.Size(93, 45);
             this.randomGeneratedNameButton.TabIndex = 0;
             this.randomGeneratedNameButton.Text = "GENERATE";
-            this.randomGeneratedNameButton.UseVisualStyleBackColor = true;
+            this.randomGeneratedNameButton.UseVisualStyleBackColor = false;
             this.randomGeneratedNameButton.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // saveNameButton
-            // 
-            this.saveNameButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.saveNameButton.Location = new System.Drawing.Point(12, 86);
-            this.saveNameButton.Name = "saveNameButton";
-            this.saveNameButton.Size = new System.Drawing.Size(93, 36);
-            this.saveNameButton.TabIndex = 1;
-            this.saveNameButton.Text = "SAVE NAME";
-            this.saveNameButton.UseVisualStyleBackColor = false;
-            this.saveNameButton.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // nameGeneratedButton
-            // 
-            this.nameGeneratedButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.nameGeneratedButton.Location = new System.Drawing.Point(128, 42);
-            this.nameGeneratedButton.Name = "nameGeneratedButton";
-            this.nameGeneratedButton.Size = new System.Drawing.Size(172, 80);
-            this.nameGeneratedButton.TabIndex = 2;
-            this.nameGeneratedButton.Text = "[No name generated]";
-            this.nameGeneratedButton.UseVisualStyleBackColor = true;
-            this.nameGeneratedButton.Click += new System.EventHandler(this.nameGeneratedButton_Click);
             // 
             // menuStrip1
             // 
@@ -106,13 +86,25 @@
             this.loadToolStripMenuItem.Text = "Load";
             this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
             // 
+            // nameGeneratedTextBox
+            // 
+            this.nameGeneratedTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.nameGeneratedTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nameGeneratedTextBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameGeneratedTextBox.Location = new System.Drawing.Point(112, 49);
+            this.nameGeneratedTextBox.Name = "nameGeneratedTextBox";
+            this.nameGeneratedTextBox.ReadOnly = true;
+            this.nameGeneratedTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.nameGeneratedTextBox.Size = new System.Drawing.Size(188, 25);
+            this.nameGeneratedTextBox.TabIndex = 4;
+            this.nameGeneratedTextBox.Text = "";
+            // 
             // nameGeneratorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(312, 145);
-            this.Controls.Add(this.nameGeneratedButton);
-            this.Controls.Add(this.saveNameButton);
+            this.ClientSize = new System.Drawing.Size(312, 90);
+            this.Controls.Add(this.nameGeneratedTextBox);
             this.Controls.Add(this.randomGeneratedNameButton);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -129,11 +121,10 @@
         #endregion
 
         private System.Windows.Forms.Button randomGeneratedNameButton;
-        private System.Windows.Forms.Button saveNameButton;
-        private System.Windows.Forms.Button nameGeneratedButton;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox nameGeneratedTextBox;
     }
 }
